58dfac47b743ea0e24922a8a8ef2bed9828a8fce
Pierre-Arnaud Delsaut <diplomegalo@hotmail.com>
2025-09-25 12:39:57 +0200

    Split the playbook in os-base and ide-base

--- roles/nvim/template/telescope.lua
+++ roles/nvim/template/telescope.lua
@@ -1,39 +1,44 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    defaults = {
-      prompt_prefix = "> ",
-      selection_caret = "> ",
-      layout_strategy = "horizontal",
-      sorting_strategy = "ascending",
-      layout_config = {
-        prompt_position = "top",
-      },
-      winblend = 0,
-    },
-  },
-  keys = {
-    {
-      "<leader>ff",
-      function()
-        require("telescope.builtin").find_files()
-      end,
-      desc = "Find Files",
-    },
-    {
-      "<leader>fb",
-      function()
-        require("telescope.builtin").buffers()
-      end,
-      desc = "Find Buffers",
-    },
-    {
-      "<leader>fk",
-      function()
-        require("telescope.builtin").keymaps()
-      end,
-      desc = "Find Keymaps",
-    },
-  },
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		defaults = {
+			prompt_prefix = "> ",
+			selection_caret = "> ",
+			layout_strategy = "horizontal",
+			sorting_strategy = "ascending",
+			layout_config = {
+				prompt_position = "top",
+			},
+			winblend = 0,
+		},
+		pickers = {
+			find_files = {
+				hidden = true,
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+			desc = "Find Buffers",
+		},
+		{
+			"<leader>fk",
+			function()
+				require("telescope.builtin").keymaps()
+			end,
+			desc = "Find Keymaps",
+		},
+	},
 }
